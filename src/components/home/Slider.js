cc28ddaf64afa6563d107d792d56db63dc7306e7
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-04-21 15:09:35 +0300

    promise last touches

--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -2,10 +2,9 @@ import React from "react";
 import { Slide } from "react-slideshow-image";
 import slide1 from "../../assets/images/slide1.jpg";
 import slide2 from "../../assets/images/slide2.jpg";
-import slide3 from "../../assets/images/slide3.jpg";
-import slide4 from "../../assets/images/slide4.jpg";
+import slide5 from "../../assets/images/slide5.jpg";
 
-const slideImages = [slide1, slide2, slide4];
+const slideImages = [slide1, slide2, slide5];
 
 const properties = {
   duration: 3000,
